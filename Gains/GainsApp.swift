706bee832973dbbc430865df60fc0b4a261c3693
rahulranjan404 <rahulranjan.debugs@gmail.com>
2026-06-07 03:06:45 +0530

    Initial Commit

---
+++ Gains/GainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GainsApp.swift
+//  Gains
+//
+//  Created by Rahul on 06/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct GainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
